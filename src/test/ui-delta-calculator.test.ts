903104c6f21d7fcb3dcbb6ba61f1ed58c65e92e1
Nomi <itsnomihere@gmail.com>
2024-07-02 21:18:42 +1000

    Refactor method should pass the test for 611-Bug

--- src/test/ui-delta-calculator.test.ts
+++ src/test/ui-delta-calculator.test.ts
@@ -7,7 +7,14 @@ beforeEach(() => {
   calculator = new UIDeltaCalculator();
 });
 
-test('the pre test data should output post test data', () => {
+test('the pre test data should output post test data - refactored method', () => {
   // Use the preTestData and postOutputData variables in your test
-  expect(calculator.calculateDelta(preTestData)).toBe(postOutputData);
+  expect(calculator.calculateDelta(preTestData)).toEqual(postOutputData);
 });
+
+
+test('the pre test data should output post test data - unrefactored method', () => {
+  // Use the preTestData and postOutputData variables in your test
+  expect(calculator.prepareAdminUIStackedDataset(preTestData)).toEqual(postOutputData);
+});
+
