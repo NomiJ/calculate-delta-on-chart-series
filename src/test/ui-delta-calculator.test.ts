884c5661c9e76200a5c3b923b387affbdd8d840e
Nomi <itsnomihere@gmail.com>
2024-07-02 22:04:11 +1000

    Refactor file names in test file

--- src/test/ui-delta-calculator.test.ts
+++ src/test/ui-delta-calculator.test.ts
@@ -1,6 +1,6 @@
 import UIDeltaCalculator from '../ui-delta-calculator';
-const preTestData = require('./611-prefix.pre.test.data.json');
-const postOutputData = require('./611-prefix.post.test.data.json');
+const preTestData = require('./611.pre.test.data.json');
+const postOutputData = require('./611.post.test.data.json');
 
 let calculator:UIDeltaCalculator;
 beforeEach(() => {
@@ -8,12 +8,11 @@ beforeEach(() => {
 });
 
 test('the pre test data should output post test data - refactored method', () => {
-  // Use the preTestData and postOutputData variables in your test
-  expect(calculator.calculateDelta(preTestData)).toEqual(postOutputData);
+  const calculatedDataSet = calculator.calculateDelta(preTestData)
+  expect(calculatedDataSet).toEqual(postOutputData);
 });
 
 test('the pre test data should output post test data - unrefactored method', () => {
-  // Use the preTestData and postOutputData variables in your test
   const calculatedDataSet = calculator.prepareAdminUIStackedDataset(preTestData)
   expect(calculatedDataSet).toEqual(postOutputData);
 });
